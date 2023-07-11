6359bdc2d90777a91d9fd6d1ac9ad6673b37ee1c
nghia <selekon297@gmail.com>
2023-07-11 14:37:08 +0700

    add config and asset

--- src/api/apiConfig.js
+++ src/api/apiConfig.js
@@ -1,4 +1,8 @@
 const apiConfig = {
     baseUrl: 'https://api.themoviedb.org/3/',
-    apiKey: '',
+    apiKey: '1a1c26f87c3562e640ddb8cdb3770552',
+    originalImage: (imgPath) => `https://image.tmdb.org/t/p/original/${imgPath}`,
+    w500Image: (imgPath) => `https://image.tmdb.org/t/p/w500/${imgPath}`,
 }
+
+export default apiConfig;
