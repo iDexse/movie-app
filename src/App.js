0ee9e9119ce8d46b48de2a0c47c46885c02fb008
nghia <selekon297@gmail.com>
2023-07-12 15:19:20 +0700

    make header

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
+import "swiper/swiper-bundle.cjs";
+import "./asset/boxicons-2.1.0/css/boxicons.min.css";
 import "./App.scss";
 
+import { BrowserRouter, Route, } from "react-router-dom";
+
+import Header from "./components/header/Header";
+import Footer from "./components/footer/Footer";
+
+import Routers from "./config/Routers";
+
 function App() {
-  return <div>App</div>;
+  return (
+    <BrowserRouter>
+      <Route
+        render={(props) => (
+          <>
+            <Header {...props} />
+            <Routers />
+            <Footer />
+          </>
+        )}
+      />
+    </BrowserRouter>
+  );
 }
 
 export default App;
